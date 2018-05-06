570a63c325c7cc8a023af0a9f02f5ad06ad7afa3
Troy Reeves <troyreeves2@gmail.com>
2018-05-06 16:28:30 -0500

    Adding the entrys for when we beging to consume the darksky api

--- app/src/main/java/com/android/constructionprojectassistant/Entry.java
+++ app/src/main/java/com/android/constructionprojectassistant/Entry.java
@@ -5,4 +5,84 @@ package com.android.constructionprojectassistant;
  */
 
 public class Entry {
+    private String mEntryContent;
+
+    private String mTemp;
+
+    private String mLocation;
+
+    private String mSkyDescription;
+
+    private String mSkyIconText;
+
+
+
+    public Entry(String skyDescription, String skyIconText, String temp) {
+
+        mSkyDescription = skyDescription;
+
+        mSkyIconText = skyIconText;
+
+        mTemp = temp;
+
+        mEntryContent = "";
+
+    }
+
+    public void setEntryContent(String entryContent) {
+
+        mEntryContent = entryContent;
+
+    }
+
+    public String getTemp() {
+
+        return mTemp;
+
+    }
+
+    public void setTemp(String temp) {
+
+        mTemp = temp;
+
+    }
+
+    public String getLocation() {
+
+        return mLocation;
+
+    }
+
+    public void setLocation(String location) {
+
+        mLocation = location;
+
+    }
+
+    public String getSkyDescription() {
+
+        return mSkyDescription;
+
+    }
+
+    public void setSkyDescription(String skyDescription) {
+
+        mSkyDescription = skyDescription;
+
+    }
+
+    public String getSkyIconText() {
+
+        return mSkyIconText;
+
+    }
+
+    public void setSkyIconText(String skyIconText) {
+
+        mSkyIconText = skyIconText;
+
+    }
+
 }
+
+
