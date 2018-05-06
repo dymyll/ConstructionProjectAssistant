892cc22124933a5e8eb394e13858ed1c5e2b0d93
Troy Reeves <troyreeves2@gmail.com>
2018-05-06 16:53:45 -0500

    Declaring the variables I will be calling to for the api and inserting my secret code so we will have permission to do it

---
+++ app/src/main/java/com/android/constructionprojectassistant/DarkSkyFetchr.java
@@ -0,0 +1,34 @@
+package com.android.constructionprojectassistant;
+
+import android.support.v4.util.Pair;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by troyr on 5/6/2018.
+ */
+
+public class DarkSkyFetchr {
+
+    private static final String TAG = "DarkSkyFetchr";
+
+    private static final String API_KEY = "d2439039bc2c32250968441a5872be5c";
+
+    private String mLat;
+
+    private String mLong;
+
+
+}
+
+
